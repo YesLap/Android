38c055a4f3f4a95849e2e8ed94217c105aa6c527
Rodrigo Shinoda <rodrigo.viegas@ulbra.inf.br>
2017-10-06 21:38:34 -0300

    1.0.4

--- app/src/main/java/com/sendlook/yeslap/UserProfileActivity.java
+++ app/src/main/java/com/sendlook/yeslap/UserProfileActivity.java
@@ -19,6 +19,9 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     @Override
